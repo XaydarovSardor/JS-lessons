4098faacb6fb20752be404cb938ad4b1af83d28e
XaydarovSardor <birbalobirbattar@gmail.com>
2024-11-07 10:32:15 +0500

    homework finished

--- lesson-5(scopes in js)/app.js
+++ lesson-5(scopes in js)/app.js
@@ -4,9 +4,9 @@ while (isPublic !== "true" && isPublic !== "false") {
 }
 let numberOfBooks;
 do {
-    numberOfBooks = +prompt("Nechta kitob o'qidingiz?")
-} while (numberOfBooks == 0 || isNaN(numberOfBooks ) || numberOfBooks<0);
-isPublic= isPublic==="true"
+    numberOfBooks = Number(prompt("Nechta kitob o'qidingiz?").trim())
+} while (numberOfBooks == 0 || isNaN(numberOfBooks) || numberOfBooks < 0);
+isPublic = isPublic === "true"
 let booksDB = {
     count: numberOfBooks,
     books: {},
@@ -17,18 +17,38 @@ let booksDB = {
 for (let i = 1; i <= numberOfBooks; i++) {
     let kitobNomi;
     let ball;
+    let author;
+    let janr;
     do {
-        kitobNomi = prompt(`${i}-qaysi kitobni o'qidingiz?`)
-        ball = +prompt(`${i}-o'qigan kitobingizga nechi ball berasiz?`)
-    } while (!isNaN(kitobNomi) || kitobNomi==0 || isNaN(ball) || ball==0 || ball<0);
-    booksDB.books[kitobNomi] = ball
+        kitobNomi = prompt(`${i}-qaysi kitobni o'qidingiz?`).trim()
+} while (kitobNomi==0 || !isNaN(kitobNomi));
+    do {
+        author = prompt(`${i}-o'qigan kitobingizning muallifi kim?`).trim()
+    } while (author==0 || !isNaN(author));
+    do {
+        janr = prompt(`${i}-kitobingiz qanday janrda?`).trim()
+    } while (kitobNomi==0 || !isNaN(kitobNomi));
+    do {
+    ball = Number(prompt(`${i}-o'qigan kitobingizga nechi ball berasiz?`).trim())
+    } while (isNaN(ball) || ball===null || ball=="" || ball<0);
+    booksDB.books[`${kitobNomi}`] =ball
+    booksDB.genres[`${kitobNomi}`] =janr
+    booksDB.authors[`${kitobNomi}`] =author
+    
+    if (booksDB.isPublic == true) {
+        console.log(booksDB);
+        console.log(`Sizning ${i}-o'qigan kitobingiz nomi - (${kitobNomi}), uning muallifi - (${author}), u - (${janr}) janrida va unga - (${ball}) ball berdingiz`);
+        let kitoblarSoni = booksDB.count
+        if (kitoblarSoni > 5) {
+            console.log("Siz kitobxon ekansiz");
+        } else if (kitoblarSoni < 5 && kitoblarSoni >= 3) {
+            console.log("Siz bazida kitob o'qir ekansiz");
+        } else if (kitoblarSoni < 3) {
+            console.log("Siz kam kitob o'qir ekansiz");
+        }
+    } else {
+        console.log("Qiymatlar chiqishi uchun isPublicga (true) qiymatini kiritishingiz kerak");
+    }
 }
-console.log(booksDB);
-let kitoblarSoni = booksDB.count
-if(kitoblarSoni>5){
-    console.log("Siz kitobxon ekansiz");    
-}else if(kitoblarSoni<5 && kitoblarSoni>=3){
-    console.log("Siz bazida kitob o'qir ekansiz");
-}else if(kitoblarSoni<3){
-    console.log("Siz kam kitob o'qir ekansiz");
-}
+
+
