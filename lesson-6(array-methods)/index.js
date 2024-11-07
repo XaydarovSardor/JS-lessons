dd1be0fe30eb47d7833e89bcffd77c323f658098
XaydarovSardor <birbalobirbattar@gmail.com>
2024-11-07 11:31:04 +0500

    added

---
+++ lesson-6(array-methods)/index.js
@@ -0,0 +1,70 @@
+//  1
+// let fruits = []
+// fruits.push("Olma", "Banan", "Apelsin")
+// fruits.shift()
+// fruits.unshift("Anor")
+// fruits.pop()
+// console.log(fruits);
+//  2
+// let numbers = [10,20,30,40,50];
+// numbers.pop();
+// numbers[2]= 35;
+// numbers.shift()
+// console.log(numbers);
+//  3
+// let queue = []
+// queue.unshift("Ali", "Vali")
+// queue.push("Shodi");
+// queue.shift()
+// queue.unshift("Jasur")
+// console.log(queue);
+//  4
+// let numbers = [2,4,6,8]
+// numbers.unshift(1)
+// numbers.push(11)
+// numbers.shift()
+// numbers.pop()
+// console.log(numbers);
+//  5
+// let workers = ["John", "Mike", "Emma", "Alice"]
+// workers.push("Sally")
+// workers.shift()
+// workers.unshift("Kate")
+// workers.pop()
+// console.log(workers); // Natija: Kate, Mike, Emma, Alice
+//  6
+// let fruits = ["Olma", "Banan", "Apelsin", "Anor", "Qulupnay", "Kivi"]
+// let fruits2 = fruits.slice(1,4);
+// console.log(fruits2);
+//  7
+// let books = ["Tarix", "Matematika", "Fizika", "Geografiya", "Kimyo"]
+// books[2] = "Biologiya"
+// console.log(books);
+//  8
+// let firstGroup = ["Ali", "Vali", "Salim"]
+// let secondGroup = ["Zafar", "Gulnoza", "Shirin"]
+// let overallGorup = firstGroup.concat(secondGroup)
+// console.log(overallGorup);
+//  9
+// let colors = ["qizil", "ko'k", "yashil", "sariq", "oq"]
+// for(color of colors){
+//     console.log(color);
+// }
+// colors.forEach(color =>{
+//     console.log(color);
+// })
+//  10
+// let animals = ["It", "Mushuk", "Quyon", "Ot", "Sigir"]
+// animals.splice(2,2,"Fil", "Tovuq")
+// console.log(animals);
+//  11
+// let numbers = [1,2,3,4,5,6,7,8,9,10,]
+// let newNumbers = numbers.splice(numbers.length/2);
+// console.log(numbers);
+// console.log(newNumbers);
+//  12
+let salaries = [200, 500, 700, 800, 1500]
+console.log(salaries);
+for (number of salaries) {
+    console.log(number * 2);
+}
